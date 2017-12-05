c26b55cfda6ed63706b80d17a6496b0824c714e6
CST2335 <jain0022@algonquinlive.com>
2017-12-05 14:56:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/geetajain/automobile/GasRecord.java
@@ -0,0 +1,13 @@
+package com.example.geetajain.automobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GasRecord extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gas_record);
+    }
+}
---
+++ app/src/main/java/com/example/geetajain/automobile/TimeActivity.java
@@ -0,0 +1,19 @@
+package com.example.geetajain.automobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time);
+    }
+    public void clickedOk(View view) {
+    }
+
+    public void clickedCancel(View view) {
+    }
+}
